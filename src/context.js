adc1b3a661d0daeaa24b0b171298edaa05b8e531
chicori3 <chicori3@naver.com>
2020-10-26 21:45:05 +0900

    번역 part 2

--- src/context.js
+++ src/context.js
@@ -5,7 +5,11 @@ const LangContext = React.createContext();
 const Lang = ({ defaultLang, children, translations }) => {
   const [lang, setLang] = useState(defaultLang);
   const hyperTranslate = (text) => {
-    if (lang === defaultLang) return text;
+    if (lang === defaultLang) {
+      return text;
+    } else {
+      return translations[lang][text];
+    }
   };
   return (
     <LangContext.Provider value={{ setLang, t: hyperTranslate }}>
